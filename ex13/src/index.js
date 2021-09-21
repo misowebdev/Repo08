ce12848e6bbcf55fa9393fcd2a7e11a077253996
misokralj <miki.t@t-com.me>
2021-09-21 02:23:18 +0200

    svi zadaci

---
+++ ex13/src/index.js
@@ -0,0 +1,11 @@
+// Only change code below this line
+
+function randomRangeNumber(minNumber, maxNumber) {
+    return Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
+}
+
+randomRangeNumber(2, 7);
+
+// Only change code above this line
+
+module.exports = randomRangeNumber;
